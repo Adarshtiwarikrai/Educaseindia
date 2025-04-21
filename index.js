807c9e06b11f254b8813587117128393c7a6efce
Adarshtiwarikrai <144175271+Adarshtiwarikrai@users.noreply.github.com>
2025-04-21 21:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 const port=process.env.PORT || 5000;
-app.listen(port,()=>console.log(`Listening on port ${port}`));
+app.listen(port,'0.0.0.0',()=>console.log(`Listening on port ${port}`));
 app.post('/addSchool', async (req, res) => {
     const { Name, Address, latitude, longitude } = req.body;
     if (!Name || typeof Name !== 'string' || Name.trim().length === 0) {
