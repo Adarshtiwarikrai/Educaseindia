c15019d0dc2f485becbe4245c0f74b6698bd00ad
Adarshtiwarikrai <144175271+Adarshtiwarikrai@users.noreply.github.com>
2025-04-21 22:03:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,25 +1,25 @@
 
 const { Pool } = require('pg');
 
-
-const pool = new Pool({
+const client = new Pool({
   connectionString: 'postgresql://neondb_owner:Bvk0KX3IxLlT@ep-young-violet-a5yinpzw-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
-  ssl: { rejectUnauthorized: false },   
-  idleTimeoutMillis: 0,                 
-  connectionTimeoutMillis: 10000        
+  ssl: { rejectUnauthorized: false },
+  idleTimeoutMillis: 0,
+  connectionTimeoutMillis: 10000
 });
+
 setInterval(async () => {
   try {
-    await pool.query('SELECT 1');
-    console.log("✅ Keep-alive ping sent to PostgreSQL");
+    await client.query('SELECT 1');
+    console.log(" Keep-alive ping sent to PostgreSQL");
   } catch (err) {
-    console.error("⚠️ Keep-alive failed:", err.message);
+    console.error("Keep-alive failed:", err.message);
   }
-}, 10 * 60 * 1000); 
+}, 10 * 60 * 1000);
 
 (async () => {
   try {
-    await pool.query(`
+    await client.query(`
       CREATE TABLE IF NOT EXISTS schools (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
@@ -28,10 +28,11 @@ setInterval(async () => {
         longitude FLOAT NOT NULL
       );
     `);
-    console.log("✅ Table 'schools' is ready.");
+    console.log("Table 'schools' is ready.");
   } catch (err) {
-    console.error("❌ Error setting up DB:", err.message);
+    console.error("Error setting up DB:", err.message);
   }
 })();
 
-module.exports = { pool };
+
+module.exports = { client };
